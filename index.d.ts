6d03b5fa46e955d8a13a23b6f45158c5f9ca4c5c
Maksim Balabash <maksim.balabash@gmail.com>
2021-11-21 14:48:21 +0400

    Add ts types and jsdoc comments

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,91 @@
-// findChrome({
-//     min: number,          // default=85; sets lowest chrome version to look for.
-//     max: number,          // default=undefined; sets highest chrome version to look for.
-//     downloadPath: string  // default=false; will download chrome when didn’t found locally; works only with met peer-dependency puppeteer-core.
-//   })
+export type FindChromeOptions = {
+  /**
+   * Minimal suitable Chromium version
+   * @default 85
+   * @type {number}
+   */
+  min: number
 
-// output:
-//   {
-//     executablePath: string,     // path to chrome bin
-//     browser: executablePath        // browser name + version
-//   }
+  /**
+   * Maximal suitable Chromium version
+   * @default undefined
+   * @type {number}
+   */
+  max: number
+
+  /**
+   * Settings for Chromium downloading
+   * @default undefined
+   * @type {({
+   *     path: string
+   *     revision: string | undefined
+   *   })}
+   */
+  download?: {
+    /**
+     * Path where to put downloaded Chromium
+     * @type {string}
+     */
+    path: string
+
+    /**
+     * Specific Chromium revision to download
+     * @type {string | undefined}
+     */
+    revision?: string
+  }
+}
+
+export type FindChromeOutput = {
+  /**
+   * Path to Chromium executable binary which you can use to run puppeteer
+   * @example '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+   * @type {string}
+   */
+  executablePath: string
+
+  /**
+   * Browser name and version
+   * @example 'Google Chrome 96.0.4664.45'
+   * @type {string}
+   */
+  browser: string
+}
+
+/**
+ * Find local suitable Chromium.
+ *
+ * By default it only tries to find Chromium v85+.
+ * You can configure suitable version using `min` and `max` options.
+ *
+ * It can also download Chromium if couldn't find any suitable version locally (you mast have puppeteer-core dependency in your package.json).
+ *
+ * @example
+ * ```js
+ * import findChrome from 'find-chrome-bin'
+ * const chromeInfo = findChrome()
+ * chromeInfo.browser //=> "Google Chrome 96.0.4664.45"
+ * chromeInfo.executablePath //=> "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
+ * ```
+ *
+ * @example
+ * ```js
+ * import { dirname, join } from 'path'
+ * import { fileURLToPath } from 'url'
+ * import findChrome from 'find-chrome-bin'
+ * const chromeInfo = findChrome({
+ *     min: 95,
+ *     max: 98,
+ *     download: {
+ *       path: join(dirname(fileURLToPath(import.meta.url)), 'chrome'),
+ *       revision: '938553'
+ *     }
+ * })
+ * ```
+ *
+ * @param {FindChromeOptions}
+ * @returns {Promise<FindChromeOutput>}
+ */
+export declare function findChrome(options: FindChromeOptions): Promise<FindChromeOutput>
+
+export default findChrome
